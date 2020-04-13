1eb960190dd1991fa4c4393e2291ba27aaa23d0a
vale-c <valent95@gmail.com>
2020-04-13 12:34:40 +0200

    completed challenge 11

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -48,4 +48,8 @@ skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('click', handleRageUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRageUpdate));
 
+let mousedown = false;
 progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
